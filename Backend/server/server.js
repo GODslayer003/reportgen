e71cf5e858551ee4e14b881def049310e9d6eb1d
Pranjal Kundliya <144585679+GODslayer003@users.noreply.github.com>
2025-12-11 11:12:04 +0530

    Update server.js

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -10,17 +10,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://report-generator-ritn.onrender.com"
-  ],
-  credentials: true,
-  allowedHeaders: ["Authorization", "Content-Type"],
-  methods: "GET,POST,PUT,DELETE,OPTIONS"
-}));
-
-app.options("*", cors());
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
